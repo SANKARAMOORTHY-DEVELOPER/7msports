93505e6405951294da7b7daed3311dc0890e19f1
SANKARAMOORTHY-DEVELOPER <78921146+SANKARAMOORTHY-DEVELOPER@users.noreply.github.com>
2023-09-06 00:23:12 +0530

    s

--- src/components/members/Map.jsx
+++ src/components/members/Map.jsx
@@ -16,7 +16,7 @@ const Map = () => {
       <video src={batm} autoPlay loop muted />
       {/* Video by cottonbro on pexels */}
       <div className={styled.facility__overlay}>
-       <Container>
+    
        {/* Hello world */}
  
        <article className={styled.facility__content}>
@@ -35,7 +35,7 @@ const Map = () => {
     src="https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d27318.888339532583!2d80.2113226!3d13.140119!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m0!4m3!3m2!1d13.1384426!2d80.22378929999999!5e1!3m2!1sen!2sin!4v1687551696177!5m2!1sen!2sin"
     width= "100%"
     padding-top ="25px"
-    height={300}
+    height={00}
     border="0"
  
     style={{ border: 0}}
@@ -49,7 +49,7 @@ const Map = () => {
   />
           </article>
          
-      </Container>.
+ 
       </div>
     </section>
   );
