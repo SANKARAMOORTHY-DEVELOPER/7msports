213641d45dabf192c268482fd1f1f89703aea068
SANKARAMOORTHY-DEVELOPER <78921146+SANKARAMOORTHY-DEVELOPER@users.noreply.github.com>
2023-09-06 00:15:48 +0530

    w

--- src/components/helpers/wrapper/Container.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "./Container.module.css";
-
-const Container = ({ children, className }) => {
-  return <div className={`${styled.container} ${className}`}>{children}</div>;
-};
-
-export default Container;
